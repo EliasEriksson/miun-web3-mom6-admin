d29d496d362b83afd0506a1720ee2061b3cd91fd
EliasEriksson <EliasEriksson95@gmail.com>
2021-10-07 12:32:10 +0200

    ignoring dev directories

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php include_once __DIR__ . "/config.php"; ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
+</head>
+<body>
+<?php include "$rootURL/templates/php/header.php"?>
+<main>
+
+</main>
+<?php include "$rootURL/templates/php/footer.php";?>
+</body>
+</html>
