fc701df7553ca20f58fb581bf418217014bdb0a7
EliasEriksson <EliasEriksson95@gmail.com>
2021-10-21 18:57:57 +0200

    added link to client live site.

--- public/templates/php/footer.php
+++ public/templates/php/footer.php
@@ -4,7 +4,8 @@ include_once __DIR__ . "/../../config.php";
 <footer><p>Skapad av Elias Eriksson 🄯</p>
     <a href="https://github.com/EliasEriksson/miun-web3-mom6-admin"
     >repo</a>
+    <a href="https://miun.eliaseriksson.eu/web3/moment6/client/"
+    >client</a>
     <a href="<?= $rootURL ?>/authenticate/" onclick="localStorage.clear()"
     >Logga ut</a>
-
 </footer>
